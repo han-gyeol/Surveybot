2b2c2c988cd6dbd8c1a5f57f2ea7b020897a04ad
Han <leook0209@gmail.com>
2017-12-07 19:58:59 +0800

    change to express

--- app.js
+++ app.js
@@ -1,53 +1,32 @@
-const express = require('express');
-const Bot = require('facebook-messenger-bot').Bot; // import Bot class
-const Elements = require('facebook-messenger-bot').Elements;
-
-const myPageAccessToken = 'EAARJA41lo9EBABA80zYkkX1A1qHCsKSZBzkNhdEuLzdMQZBsLR5KoNrgPlMNxHcfZA5ZCyBEF1kumjlgMvbRgJmsY0ShJXWbWSVdfDFMcdjkgOgr36UquT9DSCfXidrtVZCZC2Tb1sLCbaLgWD7mSvaG9eqC1PbTZB8zCKrLV7NsQZDZD';
-const myVerification = 'test';
+const express = require('express')
+const Bot = require('messenger-bot')
 
 const app = express();
-const bot = new Bot(myPageAccessToken, myVerification); // create bot instance
 
-app.use('/facebook', bot.router()); // use the router
-console.log('listenting at 3000');
+let bot = new Bot({
+  token: process.env.PAGE_ACCESS_TOKEN,
+  verify: process.env.VERIFICATION_TOKEN,
+  app_secret: process.env.APP_SECRET
+})
+
+bot.on('error', (err) => {
+  console.log(err.message)
+})
+
+bot.on('message', (payload, reply) => {
+  let text = payload.message.text
+
+  bot.getProfile(payload.sender.id, (err, profile) => {
+    if (err) throw err
+
+    reply({ text }, (err) => {
+      if (err) throw err
+
+      console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
+    })
+  })
+})
+
+app.use(bot.middleware());
 app.listen(3000);
-
-app.get('/', (req, res) => {
-    res.send('I am a chatbot');
-});
-
-bot.on('message', async message => {
-    const {sender} = message;
-
-    // get sender id
-    console.log(`Received a message from ${sender.id}`);
-
-    // fetch additional user properties
-    await sender.fetch(`first_name,last_name,profile_pic`, true); // true: use cache
-
-    console.log(`Fetched ${sender.first_name}, ${sender.last_name}, ${sender.profile_pic}`);
-
-    const {text, images, videos, location, audio} = message;
-
-    if (text) {
-        console.log(text);      // 'hey'
-    }
-
-    if (images) {
-        console.log(images);    // ['http://...', 'http://...']
-    }
-
-    if (videos) {
-        console.log(videos);    // ['http://...', 'http://...']
-    }
-
-    if (location) {
-        console.log(location);  // {title, long, lat, url}
-    }
-
-    if (audio) {
-        console.log(audio);     // url
-    }
-
-    console.log(message);       // log the message to learn about all the attributes
-});
+console.log('Echo bot server running at port 3000.')
